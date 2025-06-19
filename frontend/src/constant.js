6433ec642643fb15b39483ed5c87e83a8b434d7e
Shureed <shureedshazzad534@gmail.com>
2025-06-19 10:00:53 +0600

    solving backend issue

--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -1,7 +1,7 @@
 // export const BASE_URL = process.env.NODE_ENV === 'development' ?
 // 'http://localhost:5000' : '';
 
-export const BASE_URL = 'http://localhost:5000';//localhost 5000
+export const BASE_URL = 'https://quiznexus.onrender.com';
 
 export const USERS_URL = '/api/users';
 export const QUIZES_URL = '/api/quizes';
